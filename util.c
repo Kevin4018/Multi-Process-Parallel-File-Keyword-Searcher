56a5adf3a3d9c3a030cb819eeeb583dc0c9733f8
jerrywang631 <jerrywang631@gmail.com>
2026-04-03 14:11:10 +0800

    Update util.c
    Another typo in util.c

--- util.c
+++ util.c
@@ -31,6 +31,7 @@ ssize_t read_full(int fd, void *buf, size_t count) {
            at this time, fd[1] == 0, so the writting ends*/
         if (n == 0) {
             return total_read;
+        }
         if (n < 0) {
             if (errno == EINTR) continue;
             return -1;
