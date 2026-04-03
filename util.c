3fcef2d502863e5ecc7b4ab8e700b1d6aece711d
jerrywang631 <jerrywang631@gmail.com>
2026-04-03 13:55:40 +0800

    Modify the logic in util.c
    Add other two checks for read returns 0 and write returns 0. When any of them are 0, terminate the pipe immediately.

--- util.c
+++ util.c
@@ -7,6 +7,10 @@ ssize_t write_full(int fd, const void *buf, size_t count) {
 
     while (total_written < count) {
         ssize_t n = write(fd, ptr + total_written, count - total_written);
+        // add another check if the write returns 0, then quit immediately 
+        if (n == 0){
+            return total total_written;
+        }
         if (n < 0) {
             if (errno == EINTR) continue;
             return -1;
@@ -23,6 +27,11 @@ ssize_t read_full(int fd, void *buf, size_t count) {
 
     while (total_read < count) {
         ssize_t n = read(fd, ptr + total_read, count - total_read);
+        /* add another chekc if the pipe is closed, then read will return 0
+           at this time, fd[1] == 0, so the writting ends*/
+        if (n == 0) {
+            return total_read
+        }
         if (n < 0) {
             if (errno == EINTR) continue;
             return -1;
