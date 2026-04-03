b29739330a4956f9a1c94fb753b0c94b9fe892fc
jerrywang631 <jerrywang631@gmail.com>
2026-04-03 14:09:41 +0800

    Fix bugs in util.c
    Fix two typos in util.c

--- util.c
+++ util.c
@@ -9,7 +9,7 @@ ssize_t write_full(int fd, const void *buf, size_t count) {
         ssize_t n = write(fd, ptr + total_written, count - total_written);
         // add another check if the write returns 0, then quit immediately 
         if (n == 0){
-            return total total_written;
+            return total_written;
         }
         if (n < 0) {
             if (errno == EINTR) continue;
@@ -30,8 +30,7 @@ ssize_t read_full(int fd, void *buf, size_t count) {
         /* add another chekc if the pipe is closed, then read will return 0
            at this time, fd[1] == 0, so the writting ends*/
         if (n == 0) {
-            return total_read
-        }
+            return total_read;
         if (n < 0) {
             if (errno == EINTR) continue;
             return -1;
